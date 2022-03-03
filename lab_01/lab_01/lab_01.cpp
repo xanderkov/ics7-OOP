04af873cca9631796e382dc3ddcacb766aa06753
Kovel Alexander <xanderkovel@gmail.com>
2022-03-03 18:01:52 +0300

    Fix files

--- lab_01/lab_01/lab_01.cpp
+++
@@ -1,7 +0,0 @@
-#include "lab_01.h"
-
-lab_01::lab_01(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
-}
--- lab_01/main.cpp
+++ lab_01/main.cpp
@@ -1,10 +1,11 @@
-#include "lab_01.h"
-#include <QtWidgets/QApplication>
+#include "mainwindow.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    lab_01 w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
