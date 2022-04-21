dc5b71fbad6db307b678dde46de2bdfaf41a0aa2
Kovel Alexander <xanderkovel@gmail.com>
2022-04-21 15:04:56 +0300

    added exception realization

---
+++ lab_02/exception_realization.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "exception.h"
+ExceptionMatrix::ExceptionMatrix(const string time_info, const string file_info, const int line_info, const string errmsg)
+{
+	_errmsg = "\nFile: " + file_info + "\nLine: " + std::to_string(line_info) + "\nTime: " + time_info + "Message: " + errmsg;
+}
